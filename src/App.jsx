c0300df51d110674fd65c7b4985fa3af8b0dd373
pakomercado0517 <sissyyayle0517@hotmail.com>
2024-10-25 20:08:18 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Button, Label, TextInput } from "flowbite-react";
+
+function App() {
+  const [systolic, setSystolic] = useState("");
+  const [diastolic, setDiastolic] = useState("");
+  const [pam, setPam] = useState(null);
+
+  const calculatePAM = () => {
+    const systolicValue = parseFloat(systolic);
+    const diastolicValue = parseFloat(diastolic);
+    if (!isNaN(systolicValue) && !isNaN(diastolicValue)) {
+      const pamValue = diastolicValue + (systolicValue - diastolicValue) / 3;
+      setPam(pamValue.toFixed(2));
+    } else {
+      setPam("Valores inválidos");
+    }
+  };
+
+  return (
+    <div className="max-w-screen ">
+      <h2 className="text-center text-2xl font-semibold">
+        Calculadora de Presión Arterial Media (PAM)
+      </h2>
+      <div className="flex flex-col items-center justify-center gap-6">
+        <Label className="text-white text-center">Presión Sistólica:</Label>
+        <TextInput
+          type="number"
+          value={systolic}
+          onChange={(e) => setSystolic(e.target.value)}
+          placeholder="Ej. 125"
+          style={{ marginLeft: "10px", padding: "5px" }}
+        />
+        <Label className="text-white text-center">
+          Presión Diastólica:
+          <TextInput
+            type="number"
+            value={diastolic}
+            onChange={(e) => setDiastolic(e.target.value)}
+            placeholder="Ej. 84"
+            style={{ marginLeft: "10px", padding: "5px" }}
+          />
+        </Label>
+      </div>
+      <div></div>
+      <div className="mt-10 flex justify-center flex-col items-center max-w-screen">
+        <div>
+          <Button onClick={calculatePAM}>Calcular PAM</Button>
+        </div>
+        {pam !== null && (
+          <div className="mt-12">
+            <span className="text-xl font-semibold">PAM: {pam} mmHg</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
